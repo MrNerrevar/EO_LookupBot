d1d5a98477a8049f4ec1e605ab5cf2afa6beb88c
MrNerrevar <jake.c.brady@gmail.com>
2024-11-15 17:50:04 +0000

    Fixed compare lookup returns

--- lookups.py
+++ lookups.py
@@ -29,11 +29,14 @@ def compare_players(player_name1, player_name2):
         player1 = find_player_by_name(players, player_name1)
         player2 = find_player_by_name(players, player_name2)
 
+        player1_name = player1["name"]
+        player2_name = player2["name"]
+
         if player1 and player2:
             exp1 = player1.get('exp', 0)
             exp2 = player2.get('exp', 0)
 
-        return player1, exp1, player2, exp2
+        return player1_name, exp1, player2_name, exp2
 
 
 def print_player_info(player_name):
