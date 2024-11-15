f9d5532787427c450f7bff28a799ee6d55295a5b
MrNerrevar <jake.c.brady@gmail.com>
2024-11-15 14:29:16 +0000

    Still testing local imports

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord import app_commands
 from discord.ext import commands
-from lookups import *
+import lookups
 
 intents = discord.Intents.default()
 intents.message_content = True
