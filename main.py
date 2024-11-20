1faefdf555c66f09aa535e1c25d4b636f3f6cc3c
MrNerrevar <jake.c.brady@gmail.com>
2024-11-20 22:42:04 +0000

    Removed old import references

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import discord
 from discord import app_commands
 from discord.ext import commands
-import player_lookups as pl
 
 intents = discord.Intents.default()
 intents.message_content = True
